bae62c62558b0aae554d49d22a493c5ba91d2767
Anthony <stenbergdigeronimo@gmail.com>
2017-07-27 13:40:31 -0700

    Starting to add content from HTML files into PHP
    This is so Kerstin can have access to the same work that has been
    completed.

--- footer.php
+++ footer.php
@@ -1,3 +1,21 @@
-<?php
-
-?>
+        <!--footer starts here-->
+        <footer>
+            <div id="footer-social">
+                <i class="fa fa-linkedin-square" aria-hidden="true"></i>
+                <i class="fa fa-facebook-square" aria-hidden="true"></i>
+            </div>
+            <div id="footer-form">
+                <p>
+                    SIGN UP FO THE MUSTER REPORT
+                </p>
+                <form>
+                    <input type="email" placeholder="EMAIL ADDRESS" />
+                    <button type="submit">
+                        Sign Up
+                    </button>
+                </form>
+            </div>
+        </footer>
+    </div>
+</body>
+</html>
--- header.php
+++ header.php
@@ -1,3 +1,52 @@
-<?php
+<!DOCTYPE html>
+<html lang="en" xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <meta charset="utf-8" />
+    <title><?php get_title_tag(); ?></title>
+    <link href="<?php bloginfo('template_directory'); ?>css/reset.css" type="text/css" rel="stylesheet">
+    <link href="<?php bloginfo('template_directory'); ?>css/style.css" type="text/css" rel="stylesheet">
+    <link href="css/font-awesome.min.css" type="text/css" rel="stylesheet"> 
 
-?>
+<!--wp head function start-->
+ <?PHP wp_head(); ?>
+ <!--Die wp head function die!! NOTE: die is not the PHP die statement -->
+</head>
+<body <?PHP body_class(); ?>>
+    <div id="wrapper">
+        <header>
+            <div id="header-nav">
+                <img class="logo-upper" src="<?php bloginfo('template_directory'); ?>images/logo-smaller.png" />
+    <!--++++ Start of WordPress Nav ++++-->
+        <?PHP wp_nav_menu(); ?>
+    <!--++++ End of WordPress Nav ++++-->
+                <ul>
+                    <li>
+                        <a href="">
+                            About
+                        </a>
+                    </li>
+                    <li>
+                        <a href="">
+                            News
+                        </a>
+                    </li>
+                    <li>
+                        <a href="">
+                            Events
+                        </a>
+                    </li>
+                    <li>
+                        <a href="">
+                            Resources
+                        </a>
+                    </li>
+                </ul>
+                <div id="header-social">
+                    <i class="fa fa-linkedin-square" aria-hidden="true"></i>
+                    <i class="fa fa-facebook-square" aria-hidden="true"></i>
+                </div>
+            </div>
+            <img class="logo-lower" src="images/logo-smaller.png" />
+        <img id="header-img" src="images/header-img.png" />
+        </header>
+        <!--header ends here-->
