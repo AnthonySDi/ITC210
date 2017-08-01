be3a6163dc2e4969e6007895ac27998a332322e5
Anthony <stenbergdigeronimo@gmail.com>
2017-07-31 17:45:22 -0700

    Added PHP
    Added PHP to create the article styled page for the blog post.

--- single.php
+++ single.php
@@ -2,7 +2,19 @@
 get_header();
 ?>
 <!--This is where single.php middle +++ STARTS +++-->
-
+            <?php
+                if (have_posts()) : while (have_posts()) : the_post(); ?>
+                    <article id="article-<?php the_ID() ?>" class="">
+                        <h2>
+                                <?php the_title(); ?>
+                        </h2>
+                        <small>
+                            Published by <?php the_author(); ?> on <?PHP the_time('F jS, Y'); ?> in <?PHP the_category(', '); ?> 
+                        </small> 
+                        <?php the_post_thumbnail('large'); ?>
+                        <?php the_content(); ?>
+                    </article>
+            <?php endwhile; endif;?>
 
 
 
