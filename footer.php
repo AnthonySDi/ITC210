018ff19cebc668356990f5ada5502aecf06ac8a9
fncreative <dann.frey@me.com>
2017-08-07 16:43:46 -0700

    CSS

--- footer.php
+++ footer.php
@@ -4,13 +4,13 @@
                 <i class="fa fa-linkedin-square" aria-hidden="true"></i>
                 <i class="fa fa-facebook-square" aria-hidden="true"></i>
             </div>
-            <div id="footer-form">
+            <div id="footer-form" class='round'>
                 <p>
                     SIGN UP FOR THE MUSTER REPORT
                 </p>
                 <form>
                     <input type="email" placeholder="EMAIL ADDRESS" />
-                    <button type="submit">
+                    <button type="submit" class='round'>
                         Sign Up
                     </button>
                 </form>
