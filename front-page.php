1d83f2c58f9502208929e86bbafc7c5a3a6aeda2
Anthony <stenbergdigeronimo@gmail.com>
2017-08-10 16:45:48 -0700

    Added PHP/Stripped Content
    I removed content and added it to the WordPress site. Added PHP tag that will content on the page. This way the client doesn't need to know HTML to update content.

--- front-page.php
+++ front-page.php
@@ -3,45 +3,7 @@ get_header();
 ?>
 <!--This is where Front-page.php middle starts-->
 <!--start protosite content-->
-    <div id="main">
-        <h1>Our Mission</h1>
-        <p>
-        Foster fellowship and a unity of purpose among our Community, Veterans, and Families in King County
-        Advocate for systems change through the King County Veterans Situation Report (SITREP)
-        Ensure our military family is Connected, Healthy, Empowered, Fulfilled, and Inspired in order to enjoy a secure and prosperous civilian life
-        </p>
-        <h1>Member Spotlight</h1>
-        <p>
-        Anthony Vincent is a Vietnam-Era Veteran and brings his pro-bono Technical Project Management skills to the KCVC biosphere. He also shares a vast knowledge of Business Development, Web+Software Development and Project Management methodologies to the table as well.
-        </p>
-        <p>
-        Principal of  IT Project Management consultancy m3group, Anthony also brings 15+ years in the healthcare, small business and non-profit market sectors.  As a certified Peer Support Specialist, WRAP and IPS Recovery Coach, this humble gift brings home the true meaning of service-work in the veteran realm.
-        </p>
-        <img src="<?php bloginfo('template_directory'); ?>/images/anothony.jpg" />
-    </div>
-    <!-- this will come from the calendar-->
-    <div id="aside" class='eventcontainer'>
-        <h1>Event 1</h1>
-        <p>
-            Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Pellentesque fringilla ipsum vel quam venenatis dignissim. Orci varius natoque penatibus et magnis dis parturient montes,    nascetur ridiculus mus. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos
-        </p>
-         <h1>Event 2</h1>
-        <p>
-            Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Pellentesque fringilla ipsum vel quam venenatis dignissim. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos
-        </p>
-         <h1>Event 3</h1>
-        <p>
-            Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Pellentesque fringilla ipsum vel quam venenatis dignissim. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos
-        </p>
-    </div>
-<div class='eventbutton'>
-           <button>
-           <img src="  
-http://fpoimg.com/200x50?text=Button(Load More)&bg_color=b9b1b1&text_color=260303
- " width="200" height="50" />
- </button> 
-<!--end of stuff that will come from the calendar-->
-  </div>  
+<?php the_content(); ?>
 <!--end protosite content-->
 <!--This is where Front-page.php middle +++ ENDS ++++ -->
 <?php
