e6b67d0f0e32bb7b4147a5d9c62139514e3551ea
fncreative <dann.frey@me.com>
2017-08-12 12:24:02 -0700

    front-page changes
    added an if/while statement to pull the page content.

--- front-page.php
+++ front-page.php
@@ -3,7 +3,9 @@ get_header();
 ?>
 <!--This is where Front-page.php middle starts-->
 <!--start protosite content-->
-<?php the_content(); ?>
+<?php if(have_posts()):while(have_posts()):the_post();?>
+<?php the_content();?>
+<?php endwhile; endif; ?>
 <!--end protosite content-->
 <!--This is where Front-page.php middle +++ ENDS ++++ -->
 <?php
